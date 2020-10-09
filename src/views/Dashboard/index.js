38751f1bbfdded7ab982d94e9d267d9db422bd12
dearvae <lovevae7@gmail.com>
2020-10-09 17:38:06 +0800

    added nav bar

---
+++ src/views/Dashboard/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Nav from '../../components/common/Nav.js'
+
+export default function Dashboard() {
+ 
+  return (
+    <div>
+        <Nav />
+    </div>
+    );
+}
